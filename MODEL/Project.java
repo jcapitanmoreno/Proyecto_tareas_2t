92cf0a4eae7d19efc73340bfc799402618073152
Frenadol <you@example.com>
2024-02-25 11:41:15 +0100

    Clases definidas

--- MODEL/Project.java
+++ MODEL/Project.java
@@ -10,5 +10,44 @@ public class Project implements Iproject {
     protected String ProjectCreator;
 
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProjectCreator() {
+        return ProjectCreator;
+    }
+
+    public void setProjectCreator(String projectCreator) {
+        ProjectCreator = projectCreator;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", ProjectCreator='" + ProjectCreator + '\'' +
+                '}';
+    }
+
+    public Project(String name, String description, String projectCreator) {
+        this.name = name;
+        this.description = description;
+        ProjectCreator = projectCreator;
+
+
+    }
 }
--- MODEL/Task.java
+++ MODEL/Task.java
@@ -12,4 +12,73 @@ public class Task {
 
     protected TaskStatus taskStatus;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getStarterDate() {
+        return starterDate;
+    }
+
+    public void setStarterDate(LocalDate starterDate) {
+        this.starterDate = starterDate;
+    }
+
+    public LocalDate getLimitDate() {
+        return limitDate;
+    }
+
+    public void setLimitDate(LocalDate limitDate) {
+        this.limitDate = limitDate;
+    }
+
+    public String getProjectColaborator() {
+        return projectColaborator;
+    }
+
+    public void setProjectColaborator(String projectColaborator) {
+        this.projectColaborator = projectColaborator;
+    }
+
+    public TaskStatus getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(TaskStatus taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", starterDate=" + starterDate +
+                ", limitDate=" + limitDate +
+                ", projectColaborator='" + projectColaborator + '\'' +
+                ", taskStatus=" + taskStatus +
+                '}';
+    }
+
+    public Task(String name, String description, LocalDate starterDate, LocalDate limitDate, String projectColaborator, TaskStatus taskStatus) {
+        this.name = name;
+        this.description = description;
+        this.starterDate = starterDate;
+        this.limitDate = limitDate;
+        this.projectColaborator = projectColaborator;
+        this.taskStatus = taskStatus;
+
+    }
 }
