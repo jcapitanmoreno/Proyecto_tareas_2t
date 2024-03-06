db9be3130b13bcc1c78717dbf2208fe5dfe27ecb
Frenadol <you@example.com>
2024-03-07 00:15:27 +0100

    Mas controladores echos(Login controller aunque falta por refinarlo)

--- CONTROLLER/LogInController.java
+++ CONTROLLER/LogInController.java
@@ -10,8 +10,8 @@ import VIEW.MainView;
 import java.security.NoSuchAlgorithmException;
 
 public class LogInController implements ILoginController {
-    MainView mainView=new MainView();
-    private LogInView logInView;
+    MainView mainView = new MainView();
+    LogInView logInView = new LogInView();
 
     public LogInController(LogInView logInView) {
         this.logInView = logInView;
@@ -19,20 +19,20 @@ public class LogInController implements ILoginController {
 
     public void startLogInProcess() throws NoSuchAlgorithmException {
         String option = logInView.chooseLogIn();
-        switch (option.toLowerCase()) {
-            case "iniciar":
-                String username = logInView.enterUsername();
-                String password = Security.hashPassword(logInView.enterPassword());
-                System.out.println((password));
-
-                System.out.println("Inicio de sesión con usuario: " + username + " y contraseña: " + password);
-                break;
-            case "volver":
+        do {
+            switch (option.toLowerCase()) {
+                case "iniciar":
+                    logInView.IniciarSesion();
+                    break;
+                case "volver":
                     mainView.chooseOption();
-                break;
-            default:
-                System.out.println("Opción no válida, por favor intente de nuevo.");
-                break;
-        }
+                    break;
+                default:
+                    logInView.Error();
+                    break;
+            }
+
+
+        } while (option != "iniciar" || option != "volver") ;
     }
 }
--- VIEW/SesionView.java
+++ VIEW/SesionView.java
@@ -8,12 +8,14 @@ public class SesionView implements ISesionView {
 
     @Override
     public int chooseoption() {
+
         System.out.println("\t╔══════════════════════════════════╗\t");
         System.out.println("\t║ Pulse 1 para iniciar sesión.     ║\t");
         System.out.println("\t║ Pulse 2 para crear usuario.      ║\t");
         System.out.println("\t║ Pulse 3 para cerrar el programa. ║\t");
         System.out.println("\t╚══════════════════════════════════╝\t");
         return Teclado.leerEntero("Elige una opción: ");
+
     }
 
     public void errorOption() {
