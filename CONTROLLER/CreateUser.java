b69a66e4edcc5ae78079134827b7578ae3fd7635
Frenadol <fernandoramajimenez@gmail.com>
2024-03-12 13:51:26 +0100

    Find user

--- CONTROLLER/CreateUser.java
+++ CONTROLLER/CreateUser.java
@@ -5,27 +5,43 @@ import MODEL.User;
 import VIEW.CreateUserView;
 
 
-import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class CreateUser {
     private List<User> users;
+    CreateUserView createUserView = new CreateUserView();
 
 
     public boolean createUser() {
-        CreateUserView view = new CreateUserView();
         boolean userAdded = false;
-        User u = view.createUser();
+        User u = createUserView.createUser();
         RepoUsers ru = RepoUsers.getInstance();
-        ru.add(u);
-        if (ru.add(u)!=null) {
+        if (ru.add(u) != null) {
             userAdded = true;
-
         }
         System.out.println(ru.save());
         return userAdded;
-
+    }
+    public User findUser(String username, String email) {
+        User foundUser = null;
+        boolean isFound = false;
+        for (User user : this.users) {
+            if (user.getUser().equals(username) || user.getMail().equals(email)) {
+                foundUser = user;
+                isFound = true;
+            }
+            if (isFound) {
+                break;
+            }
+        }
+        return foundUser;
     }
 
 
+    public boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
+        return pattern.matcher(email).matches();
+
+    }
 }
--- INTERFACES/ICreateUserView.java
+++ INTERFACES/ICreateUserView.java
@@ -4,11 +4,12 @@ import MODEL.User;
 
 public interface ICreateUserView {
     int createInformation();
+
     User createUser();
-    void errorNameUser();
-    void errorUsernameUser();
-    void errorPasswordUser();
-    void errorSameUsernameUser();
-    void errorSameEmailUser();
-    void errorEmail();
+
+;
+
+    String getNewEmail();
+
+    String createEmail();
 }
